c189edbfc7c57afaa0ca3e19e9a8a91b3f4cb9b1
Dimitri Klopfstein <dimitri.kft@gmail.com>
2024-03-07 15:46:18 +0100

    add Dim

---
+++ TessaKenzaProj/DimView.swift
@@ -0,0 +1,18 @@
+//
+//  DimView.swift
+//  TessaKenzaProj
+//
+//  Created by dimitri on 07/03/2024.
+//
+
+import SwiftUI
+
+struct DimView: View {
+    var body: some View {
+        Text("Dimitri!")
+    }
+}
+
+#Preview {
+    DimView()
+}
