4e7ff5b8e1e45443ea77dde8a3bc44a118d43977
Dimitri Klopfstein <dimitri.kft@gmail.com>
2024-03-07 15:40:18 +0100

    first

---
+++ TessaKenzaProj/TessaKenzaProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  TessaKenzaProjApp.swift
+//  TessaKenzaProj
+//
+//  Created by dimitri on 07/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TessaKenzaProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
