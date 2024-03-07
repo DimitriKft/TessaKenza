feb64521a3e2492dc0fb1a2e67a5cba498a9d144
Dimitri Klopfstein <dimitri.kft@gmail.com>
2024-03-07 15:42:16 +0100

    add test

---
+++ TessaKenzaProj/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  TessaKenzaProj
+//
+//  Created by dimitri on 07/03/2024.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("vue de test")
+    }
+}
+
+#Preview {
+    TestView()
+}
